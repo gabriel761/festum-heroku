7ddd193fa8a714938d5b4dfdf53390db4c12722d
joao gabriel <jg.7651@gmail.com>
2022-11-12 16:59:58 -0300

    com ssl agora

--- infra/database.js
+++ infra/database.js
@@ -5,7 +5,11 @@ const db = pgp({
     host: "ec2-44-194-92-192.compute-1.amazonaws.com",
     port: 5432,
     database: 'd85hrgsto4u8k2',
-    connectionString: "postgres://vztihfmjvpncfr:60f8dfddd7b6132cdc96987443722e61cef7454870290d24cd42a195742e981b@ec2-44-194-92-192.compute-1.amazonaws.com:5432/d85hrgsto4u8k2"
+    connectionString: "postgres://vztihfmjvpncfr:60f8dfddd7b6132cdc96987443722e61cef7454870290d24cd42a195742e981b@ec2-44-194-92-192.compute-1.amazonaws.com:5432/d85hrgsto4u8k2",
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
+
 
 module.exports = db
