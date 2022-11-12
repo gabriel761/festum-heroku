b5fda49fa679ecba65c7cfc3855b310b19c41a31
joao gabriel <jg.7651@gmail.com>
2022-11-12 16:16:58 -0300

    mais uma tentativa de botar o app no ar no heroku

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const app = express()
 const cors = require("cors")
 const bodyParser = require("body-parser")
 
+
+ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors())
 
 app.use('/', router)
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log("listening to port 5000")
 })
