d002f2327468a9f98c4e2d5f73b06fd4ccfa6b7e
joao gabriel <jg.7651@gmail.com>
2022-11-12 16:36:40 -0300

    botando app no heroku

--- infra/database.js
+++ infra/database.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')();
 const db = pgp({
     user: 'vztihfmjvpncfr',
     password: '60f8dfddd7b6132cdc96987443722e61cef7454870290d24cd42a195742e981b',
-    host: 'postgres://vztihfmjvpncfr:60f8dfddd7b6132cdc96987443722e61cef7454870290d24cd42a195742e981b@ec2-44-194-92-192.compute-1.amazonaws.com:5432/d85hrgsto4u8k2',
+    host: "ec2-44-194-92-192.compute-1.amazonaws.com",
     port: 5432,
     database: 'd85hrgsto4u8k2'
 })
