6d0276c81993419573a64fc340eb7a5d2ef33ef1
joao gabriel <jg.7651@gmail.com>
2022-11-12 18:38:23 -0300

    agora sim

--- infra/database.js
+++ infra/database.js
@@ -1,11 +1,23 @@
 const pgp = require('pg-promise')(); 
+// const db = pgp({
+//     user: 'postgres',
+//     password: 'thebest1761',
+//     host: "localhost",
+//     port: 5432,
+//     database: 'festa-on'
+// })
+
 const db = pgp({
-    user: 'postgres',
-    password: 'thebest1761',
-    host: "localhost",
+    user: 'vztihfmjvpncfr',
+    password: '60f8dfddd7b6132cdc96987443722e61cef7454870290d24cd42a195742e981b',
+    host: "ec2-44-194-92-192.compute-1.amazonaws.com",
     port: 5432,
-    database: 'festa-on'
+    database: 'd85hrgsto4u8k2',
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
+
 
 
 module.exports = db
