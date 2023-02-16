b96dd474d4f2b49cb59c0e1f41bb831c0c29b2fc
joao gabriel <jg.7651@gmail.com>
2023-02-16 18:58:42 -0300

    primeira conexão com railway

--- infra/database.js
+++ infra/database.js
@@ -1,11 +1,11 @@
 const pgp = require('pg-promise')(); 
-const db = pgp({
-    user: 'postgres',
-    password: 'thebest1761',
-    host: "localhost",
-    port: 5432,
-    database: 'festa-on'
-})
+// const db = pgp({
+//     user: 'postgres',
+//     password: 'thebest1761',
+//     host: "localhost",
+//     port: 5432,
+//     database: 'festa-on'
+// })
 
 // const db = pgp({
 //     user: 'vztihfmjvpncfr',
@@ -17,6 +17,17 @@ const db = pgp({
 //         rejectUnauthorized: false
 //     }
 // })
+const db = pgp({
+    user: 'postgres',
+    password: 'w4IwZ82zpgX8jqWPS02f',
+    host: "containers-us-west-61.railway.app",
+    port: 6408,
+    database: 'railway',
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+
 
 
 
