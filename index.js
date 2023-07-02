8fbc638658683ebe3e9bf0ebec27cc4aa1488236
joao gabriel <jg.7651@gmail.com>
2023-07-02 17:19:30 -0300

    corrigindo erro de CORS

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const bodyParser = require("body-parser")
  
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(cors())
+app.use(cors({
+    origin:"http://festum.profissional.ws"
+}))
 
 app.use('/', router)
 
