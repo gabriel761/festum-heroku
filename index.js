c396fc65f13b0d0b28ca680db788df62a6ec1ba9
joao gabriel <jg.7651@gmail.com>
2023-07-03 11:10:16 -0300

    primeiro commit com o cors corrigido

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser")
  app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://festum-site.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
