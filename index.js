4c7a2af7e742e401b0a55d5e4447c240397e79e6
joao gabriel <jg.7651@gmail.com>
2023-07-03 09:00:53 -0300

    cors com origem

--- index.js
+++ index.js
@@ -4,28 +4,17 @@ const app = express()
 const cors = require("cors")
 const bodyParser = require("body-parser")
 
-app.use (function (req, res, next) {
-    console.log ("inside middleware");
-    next();
-  });
  const port = process.env.PORT || 5000
  
- app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "festum-site.vercel.app"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use (function (req, res, next) {
-    console.log ("inside middleware");
-    next();
-  });
-// app.use(cors({
-//     allowedHeaders: "*",
-//     allowedMethods: "*",
-//     origin: "*"
-// }))
+
+app.use(cors({
+    allowedHeaders: "*",
+    allowedMethods: "*",
+    origin: "https://festum-site.vercel.app/"
+}))
 
 app.use('/', router)
 
