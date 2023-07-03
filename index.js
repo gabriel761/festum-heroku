01da72a1a6e52996f85e5e2342fb5008a3057e8f
joao gabriel <jg.7651@gmail.com>
2023-07-03 09:53:01 -0300

    cors antes de tudo

--- index.js
+++ index.js
@@ -6,22 +6,17 @@ const bodyParser = require("body-parser")
 
  const port = process.env.PORT || 5000
  
-
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-
-
-const hola = function(req, res, next) {
-    console.log("are you okay?");
-    next();
-    console.log("Yes I'm okay....");
-  }
-  app.use(hola)
-app.use(cors({
+ app.use(cors({
     allowedHeaders: "*",
     allowedMethods: "*",
     origin: "*"
 }))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+
+
 
 app.use('/', router)
 
