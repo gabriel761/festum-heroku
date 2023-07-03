7de0d43e24b2da7be0a7557ebfab9d2782ad6770
joao gabriel <jg.7651@gmail.com>
2023-07-03 08:05:42 -0300

    ultima tentativa

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const bodyParser = require("body-parser")
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors({
-    origin: "http://10.0.0.103:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"]
+    allowedHeaders: "*",
+    allowedMethods: "*",
+    origin: "*"
 }))
 
 app.use('/', router)
