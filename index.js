9af6b2022cb1ce3b34393281629efb17f5d025e2
joao gabriel <jg.7651@gmail.com>
2023-07-03 08:49:48 -0300

    cors da internet com dominio especificado

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use (function (req, res, next) {
  const port = process.env.PORT || 5000
  
  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "festum-site.vercel.app"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
