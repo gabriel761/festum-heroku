79da8af31e0bad6eaa20b8e074127fbc8cabacb0
cliente <cliente@MacBook-Air-de-alugacom.local>
2024-05-14 11:27:54 -0300

    cors authorization

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.set('json spaces', 2)
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
